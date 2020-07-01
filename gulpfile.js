a227230ba3a2d642bb41b0ad01021300ef4b370a
RandomSavage <50834695+RandomSavage@users.noreply.github.com>
2020-06-30 23:18:16 -0400

    working on gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,55 +3,75 @@ const sass = require('gulp-sass')
 const autoprefixer = require('gulp-autoprefixer')
 // const concat = require('gulp-concat')
 // const babel = require('gulp-babel')
-// const watch = require('gulp-watch')
+const watch = require('gulp-watch')
 const browserSync = require('browser-sync')
 const reload = browserSync.reload
+const webpack = require('webpack')
 var exec = require('child_process').exec;
 
-gulp.task('default', ['styles', 'webpack', 'browser-sync'], () => {
-  gulp.watch('./assets/sass/**/*', ['styles'])
-  gulp.watch('./assets/js/**/*', ['webpack'])
-  gulp.watch(['./public/**/*', './public/*', '!public/js/**/.#*js', '!public/css/**/.#*css']).on('change', reload)
-})
 
-gulp.task('styles', () => {
-  gulp.src('assets/sass/**/*.scss')
-    .pipe(
-      sass({
-        outputStyle: 'compressed'
-      })
-      .on('error', sass.logError))
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions']
-    }))
-    .pipe(gulp.dest('./public/css'))
-    .pipe(browserSync.stream())
-})
 
-gulp.task('browser-sync', ['styles'], function () {
-  // THIS IS FOR SITUATIONS WHEN YOU HAVE ANOTHER SERVER RUNNING
-  // browserSync.init({
-  //   proxy: {
-  //     target: 'localhost:3000', // can be [virtual host, sub-directory, localhost with port]
-  //     ws: true // enables websockets
-  //   },
-  //   serveStatic: ['.', './public']
-  // })
+gulp.task('styles',
+  gulp.series(()=> {
+    return
+    gulp.src('assets/sass/**/*.scss')
+      .pipe(
+        sass({
+          outputStyle: 'compressed'
+        })
+        .on('error', sass.logError)
+      )
+      .pipe(autoprefixer({
+          browsers: ['last 2 versions']
+        })
+      )
+      .pipe(gulp.dest('./public/css'))
+      .pipe(browserSync.stream())
+  })
+);
 
-  browserSync.init({
-        server: './public',
-        notify: false,
-        open: false //change this to true if you want the broser to open automatically 
-    });
-})
 
-gulp.task('webpack', (cb) => {
-  exec('webpack', function (err, stdout, stderr) {
-      console.log(stdout);
-      console.log(stderr);
-      cb(err);
-    });
-})
+gulp.task('webpack',
+  gulp.series((cb) => {
+    return exec('npm run dev:webpack', function (err, stdout, stderr) {
+        console.log(stdout);
+        console.log(stderr);
+        cb(err);
+      });
+  })
+)
+
+
+// Browser-sync to get live reload and sync with mobile devices
+gulp.task(
+	'browser-sync',
+	gulp.series(function() {
+		browserSync.init({
+			server: './public',
+			notify: false,
+			open: true, //change this to true if you want the broser to open automatically
+			injectChanges: false
+		});
+	})
+);
+
+
+// This is your Default Gulp task
+gulp.task(
+	'default',
+	gulp.parallel([
+		gulp.series([
+			'webpack',
+			'styles',
+			function runningWatch() {
+        gulp.watch('./assets/js/**/*', gulp.parallel('webpack'));
+				gulp.watch('./assets/sass/**/*', gulp.parallel('styles'));
+				gulp.watch(['./public/**/*', './public/*']).on('change', reload);
+			}
+		]),
+		gulp.series(['browser-sync'])
+	])
+);
 
 // gulp.task('webpack', shell.task([
 //   'webpack'
@@ -60,3 +80,16 @@ gulp.task('webpack', (cb) => {
 // gulp.task('server', shell.task([
 //   'yarn run server'
 // ]))
+
+// gulp.task('browser-sync',
+//   gulp.series(function() {
+    // THIS IS FOR SITUATIONS WHEN YOU HAVE ANOTHER SERVER RUNNING
+    // browserSync.init({
+    //   proxy: {
+    //     target: 'localhost:3000', // can be [virtual host, sub-directory, localhost with port]
+    //     ws: true // enables websockets
+    //   },
+    //   serveStatic: ['.', './public']
+    // })
+//   })
+// )
